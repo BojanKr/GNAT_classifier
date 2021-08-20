ddc85893da2a1681b9bc9ccd726e761b4ec255ff
bk <bojankrtenic@gmail.com>
2021-08-20 20:09:05 +0200

    args

--- code/predict.py
+++ code/predict.py
@@ -2,27 +2,63 @@
 import pandas as pd
 import os
 from kNN import kNN
+import argparse
 
-job = input('Enter job name: ')
-os.mkdir(os.path.join(os.path.dirname(os.getcwd()), 'results', job))
+def main():
 
-# Load labes and sort the df
-labels = pd.read_csv(os.path.join(os.path.dirname(os.getcwd()), 'data', 'clean_cluster_labels.csv'), sep=',')
-labels['index_col'] = labels['Accession number']
-labels.set_index('index_col', inplace=True)
-# Load the file with query sequences
-query_file = input('Path to query file: ')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("input", help="Specify the file with query sequences.")
+    parser.add_argument("output", help="Specify the output file")
+    parser.add_argument("-k", help='Select k-neighbours to be considered', type=int)
+    parser.add_argument("-e", "--e_value", help="Specify E-value for BLAST.", type=int)
+    #parser.add_argument("-m", "--metric", help="Specify the measure of similarity between proteins. "
+    #                                          "The default is alignment score.")
+    #parser.add_argument("-l", "--labels", help="Select file with protein labels.")
+    #parser.add_argument("-p", "--prosite", help="Provide a list of PROSITE signatures your group of proteins" \
+    #                                           "should contain")
+    parser.add_argument("-s", "--score", help="Specify minimum alignment score to be considered for "
+                                              "evaluating neighborhood")
 
-X, y = labels[['Accession number', 'Description']], labels['Cluster numbers']
+    args = parser.parse_args()
 
-clf = kNN(k=10)
+    #job = input('Enter job name: ')
+    if not os.path.exists(args.output):
+        os.makedirs(os.path.join(args.output, 'BLAST_results'))
+    else:
+        replace_dir = input('This folder already exists. Overwrite the existing folder? [y,n] ')
+        if replace_dir == 'y'.lower():
+            pass
+        elif replace_dir == 'n'.lower():
+            pass
 
-# Fit
-clf.fit(X, y)
 
-# Predict labels
-predictions = clf.predict(X=query_file, job_name=job)
-print(predictions)
+    # Load labes and sort the df
+    labels = pd.read_csv(os.path.join(os.path.dirname(os.getcwd()), 'data', 'clean_cluster_labels.csv'), sep=',')
+    labels['index_col'] = labels['Accession number']
+    labels.set_index('index_col', inplace=True)
+
+    X, y = labels[['Accession number', 'Description']], labels['Cluster numbers']
+
+    if args.k:
+        clf = kNN(k=args.k)
+    else:
+        clf = kNN(k=10)
+
+    # Fit
+    clf.fit(X, y)
+
+    # Predict labels
+    if args.e_value:
+        predictions = clf.predict(X=args.input, output=args.output, e_value=args.e_value)
+        print(predictions)
+    else:
+        predictions = clf.predict(X=args.input, output=args.output, e_value=0.001)
+        print(predictions)
+
+
+if __name__ == '__main__':
+    main()
+
 
 
 # Test accuracy of the kNN model
